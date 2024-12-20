0136e4e3ebb9b16e1dc21285de91b58850d27872
jerrbearis2cool <68572439+jerrbearis2cool@users.noreply.github.com>
2024-12-20 16:14:14 -0600

    created host, updated ui

--- Hardware/main.py
+++ Hardware/main.py
@@ -1,6 +1,7 @@
 import pygame
 import threading
 from pocketsphinx import LiveSpeech
+from pygame.locals import QUIT, MOUSEBUTTONDOWN
 
 speech = []
 clock = pygame.time.Clock()
@@ -11,6 +12,7 @@ class Circle:
         self.x, self.y = x, y
         self.colour = colour
         self.radius = radius
+        self.active = False
 
     def increase(self, step):
         self.radius += step
@@ -26,21 +28,34 @@ def transcribe():
 screen = pygame.display.set_mode((1080, 1920), display=1)
 
 circle = Circle(screen)
-microphone = pygame.image.load("assets\\microphone.png")
+microphone = pygame.image.load("Hardware\\assets\\microphone.png")
 
 if __name__ == "__main__":
     transcribe_t = threading.Thread(target=transcribe, daemon=True)
     transcribe_t.start()
-    run = True
-    while run:
-        event = pygame.event.get()
-        print(speech)
+    running = True
+    while running:
+        events = pygame.event.get()
+        collide = pygame.Rect(1080 // 2 - 150, 1920 // 2 - 150, 300, 300)
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == MOUSEBUTTONDOWN:
+                if collide.collidepoint(event.pos):  # Check if mouse collides with rect
+                    circle.active = not circle.active
+
+        print(" ".join(speech))
         screen.fill((51, 92, 78))
 
-        circle.increase(30)
+        if circle.radius < 1920 and circle.active:
+            circle.increase(30)
+        else:
+            if circle.radius > 30:
+                circle.increase(-30)
         circle.render()
 
-        pygame.display.blit((0, 0), microphone)
-
+        
+        screen.blit(microphone, (1080 // 2 - 150, 1920 // 2 - 150))
+    
         pygame.display.flip()
         clock.tick(60)
