197d5b5453f5000868a1245f3f868b6ae49ed42a
Flaryiest <ericmzuo1@gmail.com>
2024-12-21 16:27:19 -0700

    created websocket server

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import ws from "ws"
+const wss = new ws.WebSocketServer({port: 8080})
+wss.on("connection", (ws) => {
+    console.log("new connection formed.")
+})
+
+wss.on("message", (message) => {
+    console.log("new message received")
+    console.log(message)
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message)
+        }
+    })
+})
+
+wss.on("close", () => {
+    console.log("Client Disconnected");
+}); 
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react"
+
 export default function Dashboard() {
-    
+    useEffect(() => {
+        ws.current = new WebSocket("http://localhost:8080")
+        ws.current.onopen = () => {
+            console.log("websocket opened")
+        }
+        ws.current.onmessage = (event) => {
+            console.log("received message")
+            triggerRender(prevRender => prevRender + 1)
+        }
+        ws.current.onclose = () => {
+            console.log("websocket closed")
+        }
+        ws.current.onerror = (error) => {
+            console.log(error)    
+        }
+        return () => {
+            ws.current.close()
+        }
+        
+    }, [])
+
+    return <div className="dashboard-page">
+        <div className="dashboard">
+            
+        </div>
+    </div>
 }
