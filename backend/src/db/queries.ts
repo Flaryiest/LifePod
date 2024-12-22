32e6c1af3f9d87b84373bfc79b424032153b26cb
jerrbearis2cool <68572439+jerrbearis2cool@users.noreply.github.com>
2024-12-22 16:50:25 -0600

    minor fix

--- backend/src/db/queries.ts
+++ backend/src/db/queries.ts
@@ -73,11 +73,11 @@ class Database {
             else {
                 return false
             }
+        } catch(err) {
+            console.log(err)
+            return false
         }
-    } catch(err) {
-        console.log(err)
-        return false
-    }
+        } 
 }
 
 export default Database;
