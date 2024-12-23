c0ccc56803ca5e51701e045f6893db731f14afac
Flaryiest <ericmzuo1@gmail.com>
2024-12-23 11:20:08 -0700

    built backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,9 +9,13 @@ const port = process.env.PORT || 3000;
 app.use(cors({ origin: "https://lifepod.pages.dev", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use((express.urlencoded({ extended: true })));
 app.options("*", cors());
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 app.use("/api", apiRouter);
 app.listen(port, () => {
     console.log("Port: " + String(port));
